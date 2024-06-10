fe9807bd9abe906f67fc7ff8d805149c37a21614
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-10 02:29:12 +0200

    Added debugging print

--- app/app.py
+++ app/app.py
@@ -10,6 +10,8 @@ def parse_git_diff(diff_output: str):
     '''TODO'''
     lines = diff_output.split('\n')
     summary = []
+
+    print('parse_git_diff')
     
     for line in lines:
         if line.startswith('+'):
@@ -27,13 +29,19 @@ def parse_git_diff(diff_output: str):
 
 def main() -> None:
     """Main function of the app"""
-    user = 'qte77'
-    repo = 'parse-git-log-action'
     commit_1 = 'e74867104f8292ecc125559a6164bc6542228514'
     commit_2 = 'e36027808600853749d7fa332695e4078df48b92'
+    
+    #user = 'qte77'
+    #repo = 'parse-git-log-action'
     #gh_ul = f"github.com/{user}/{repo}/compare/{commit_1}...{commit_2}"
+    
     diff_run = run(["git", "diff", "$commit_1", "$commit_2"], capture_output=True)
+    print(diff_run)
+    
     diff_output = diff_run.stdout.decode("utf-8")
+    print(diff_output)
+    
     summary = parse_git_diff(diff_output)
     print(summary)
 
