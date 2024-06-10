351353b9f20be7cf3e25b67482f05b8f90762b2d
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-10 02:16:54 +0200

    Merge dev-parse-baseline-python (#3)
    * Added title and acknowledgements to Readme
    
    * Changed main(), Added parse_git_diff()
    
    * Create parse-git-log-action.yml
    
    * Bumoed CodeQL to v2
    
    * Changed trigger gh-pages.yml to manual
    
    * Changed trigger links-fail-fast.yml to chron and manual
    
    * Delete .github/workflows/linter.yml
    
    * Create ruff.yml

--- app/app.py
+++ app/app.py
@@ -3,11 +3,39 @@
 """Entry point to the app"""
 
 from sys import exit
+import subprocess import run
+
+
+def parse_git_diff(diff_output: str):
+    '''TODO'''
+    lines = diff_output.split('\n')
+    summary = []
+    
+    for line in lines:
+        if line.startswith('+'):
+            summary.append(f"Added line: {line[1:]}")
+        elif line.startswith('-'):
+            summary.append(f"Removed line: {line[1:]}")
+        elif line.startswith('@@'):
+            hunk_info = line.split('@@')
+            original_range = hunk_info[1].strip().split(' ')[1]
+            modified_range = hunk_info[2].strip()
+            summary.append(f"Changes in lines {original_range} of the original file and {modified_range} of the modified file")
+
+    return '\n'.join(summary)
 
 
 def main() -> None:
     """Main function of the app"""
-    pass
+    user = 'qte77'
+    repo = 'parse-git-log-action'
+    commit_1 = 'e74867104f8292ecc125559a6164bc6542228514'
+    commit_2 = 'e36027808600853749d7fa332695e4078df48b92'
+    #gh_ul = f"github.com/{user}/{repo}/compare/{commit_1}...{commit_2}"
+    diff_run = run(["git", "diff", "$commit_1", "$commit_2"], capture_output=True)
+    diff_output = diff_run.stdout.decode("utf-8")
+    summary = parse_git_diff(diff_output)
+    print(summary)
 
 
 if __name__ == "__main__":
