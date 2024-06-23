19e0a9cf96d7af6d283cefe1fa6fdb05e6d5d6d6
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-23 10:37:29 +0200

    Move get_git_diff() to utils.py

--- app/app.py
+++ app/app.py
@@ -1,27 +1,36 @@
-"""Entry point to the app"""
+'''Entry point to the app'''
 
+from os import getenv
 from sys import exit
+from pathlib import Path
 from subprocess import run
-from utils import get_parsed_diff
+from utils import get_parsed_diff, get_git_diff
 
 
 def main() -> None:
-    """Main function of the app"""
+    '''Main function of the app'''
+    # TODO assert env OUT_FILE can be converted to POSIX PAth
+    OUT_FILE = Path(getenv("OUT_FILE", 'data/dummy-data.md'))
+    CMD_HIGHLIGHT = str(getenv("CMD_HIGHLIGHT", 'sh'))
+    # TODO check getenv for bool
+    DRY_RUN  = bool(getenv("CMD_HIGHLIGHT", False))
+    
     commit_1 = 'e74867104f8292ecc125559a6164bc6542228514'
     commit_2 = 'e36027808600853749d7fa332695e4078df48b92'
-    
     #user = 'qte77'
     #repo = 'parse-git-log-action'
     #gh_ul = f"github.com/{user}/{repo}/compare/{commit_1}...{commit_2}"
-    
-    diff_run = run(["git", "diff", commit_1, commit_2], capture_output=True)
-    print(diff_run)
-    
-    diff_output = diff_run.stdout.decode("utf-8")
-    print(diff_output)
 
-    summary = get_parsed_diff(diff_output)
-    print(summary)
+    summary = get_parsed_diff(get_git_diff(commit_1, commit_2))
+
+    if DRY_RUN:
+        print(summary)
+    else DRY_RUN:
+        if not OUT_FILE.parent.exists():
+          # folder needs to exist before open() context
+          OUT_FILE.parent.mkdir(parents=True, exist_ok=True)
+        with open(OUT_FILE, 'a+', newline=None, encoding='UTF8') as f:
+            f.write(summary)
 
 
 if __name__ == "__main__":
